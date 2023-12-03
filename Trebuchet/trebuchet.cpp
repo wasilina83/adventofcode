05e41a5032972c8470d753244e4092b25831bbb4
wasilina83 <angelinaengelmann@live.de>
2023-12-03 14:27:08 +0100

    added a funktion nut right jej

--- Trebuchet/trebuchet.cpp
+++ Trebuchet/trebuchet.cpp
@@ -7,36 +7,50 @@
 #include <unordered_map>
 
 
-std::unordered_map<std::string, int > spellOutDigits = {
-    {"zero", 0}, {"one",1}, {"two",2}, {"three",3}, {"four",4}, {"five",5}, {"six",6}, {"seven",7}, {"eight",8}, {"nine",9}
-};
+bool replace(std::string& str, const std::string& from, const std::string& to){
+    size_t start_pos = str.find(from);
+    if(start_pos == std::string::npos)
+        return false;
+    str.replace(start_pos, from.length(), to);
+    return true;
 
+}
 int main(){
     std::ifstream infile("input-file.txt");
     std::string line;
     int number = 0;
 
-    while (std::getline(infile, line)){
-        for (const auto& pair : spellOutDigits){
-            size_t pos = line.find(pair.first);
-            if (pos != std::string::npos){
-                firstNmberInLine = pair.second;
-                break;
-            }
-        }
-        for (auto it = spellOutDigits.rbegin(); it != spellOutDigits.rend(); ++it){
-            size_t pos = line.rfind(it->first);
-            if (pos != std::string::npos){
+    std::unordered_map<std::string, std::string> spellOutDigits = {
+    {"zero", "0"}, {"one", "1"}, {"two", "2"}, {"three", "3"}, {"four", "4"},
+    {"five", "5"}, {"six", "6"}, {"seven", "7"}, {"eight", "8"}, {"nine", "9"}
+        };
 
+    while (std::getline(infile, line)){
+        std::string NewLine = line;
+        for (const auto& pair : spellOutDigits){
+            replace(NewLine, pair.first, pair.second);
+            replace(NewLine, pair.first, pair.second);
+            replace(NewLine, pair.first, pair.second);
+            replace(NewLine, pair.first, pair.second);
+            replace(NewLine, pair.first, pair.second);
+            replace(NewLine, pair.first, pair.second);
             }
-        }
-        auto firstNumberPosition = std::find_if(line.begin(), line.end(), ::isdigit);
-        auto lastNumberPosition = std::find_if(line.rbegin(), line.rend(), ::isdigit);
+       // replace(NewLine, "one", "1");
+       // replace(NewLine, "two", "2");
+       // replace(NewLine, "three", "3");
+       // replace(NewLine, "four", "4");
+       // replace(NewLine, "five", "5");
+       // replace(NewLine, "six", "6");
+       // replace(NewLine, "seven", "7");
+       // replace(NewLine, "eight", "8");
+       // replace(NewLine, "nine", "9");
+        auto firstNumberPosition = std::find_if(NewLine.begin(), NewLine.end(), ::isdigit);
+        auto lastNumberPosition = std::find_if(NewLine.rbegin(), NewLine.rend(), ::isdigit);
         int firstNmberInLine =  *firstNumberPosition - '0';
         int lastNumberInLine = *lastNumberPosition - '0';
         int lineNumber = firstNmberInLine*10+lastNumberInLine;
         number = number+lineNumber;
-        std::cout << line << ':' << firstNmberInLine <<" & " << lastNumberInLine << '\n';
+    std::cout<<"Newline:"<<NewLine <<"\n-Line:"<< line << ':' << firstNmberInLine <<" & " << lastNumberInLine << '\n';
     }
     std::cout<< number;
 }
