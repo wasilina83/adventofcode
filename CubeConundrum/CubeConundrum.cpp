11c5eca7a11b5bbaf72baee3f3fb696cf0e63b7a
wasilina83 <angelinaengelmann@live.de>
2023-12-09 23:12:18 +0100

    finaly 1 star

--- CubeConundrum/CubeConundrum.cpp
+++ CubeConundrum/CubeConundrum.cpp
@@ -34,8 +34,36 @@ int main() {
         int failScore = 0;
 
         for (size_t i = 1; i <= line.length(); ++i) {
-            if (line[i] == ';' || line[i] == ',' || line[i] == ':') {
-                int firstPosNum = i + 2;
+		 if (line[i] == ':') {
+                    int firstPosGameID = 5;
+                    int lastPosGameID = i;
+                    std::string gameIDStr = line.substr(firstPosGameID, lastPosGameID - firstPosGameID);
+                    gameID = std::stoi(gameIDStr);
+                    std::cout << "ID: " << gameID << std::endl;
+		    int firstPosNum = i + 2;
+                    size_t spacePos = line.find(' ', firstPosNum);
+                    std::string numStr = line.substr(firstPosNum, spacePos - firstPosNum);
+                    int num = std::stoi(numStr);
+
+                    size_t firstColorPos = spacePos + 1;
+                    size_t lastColorPos = line.find(' ', firstColorPos);
+                    std::string colorStr = line.substr(firstColorPos, lastColorPos - firstColorPos - 1);
+
+                    std::cout << "Number: " << num << " Color: " << colorStr;
+
+                    // Überprüfung der Kombination aus Farbe und Anzahl
+                    if (cubeCompare(colorStr, num)) {
+                    	std::cout << " (Valid)";
+                    } else {
+                        std::cout << " (Invalid)";
+                    	failScore += 1;
+                	}
+
+                    std::cout << " Score: " << failScore << std::endl;
+
+
+		 }else if (line[i] == ','|| line[i] == ';'){
+			 int firstPosNum = i + 2;
                 size_t spacePos = line.find(' ', firstPosNum);
                 std::string numStr = line.substr(firstPosNum, spacePos - firstPosNum);
                 int num = std::stoi(numStr);
@@ -55,28 +83,14 @@ int main() {
                 }
 
                 std::cout << " Score: " << failScore << std::endl;
-
-                // Wenn es sich um die ID handelt, speichere sie für weitere Verwendungszwecke
-                if (line[i] == ':') {
-                    int firstPosGameID = 5;
-                    int lastPosGameID = i;
-                    std::string gameIDStr = line.substr(firstPosGameID, lastPosGameID - firstPosGameID);
-                    gameID = std::stoi(gameIDStr);
-                    std::cout << "ID: " << gameID << std::endl;
-
-                    // Füge gameID zur Summe nur hinzu, wenn keine ungültigen Kombinationen gefunden wurden
-                    if (failScore == 0) {
-                        sumOfPossibleGames += gameID;
+		 }else if (i == line.length() && failScore == 0){
+			sumOfPossibleGames += gameID;
                         std::cout << "Summe der möglichen Spiele: " << sumOfPossibleGames << std::endl;
-                    }
-                }
-            }
+		 }
 
-            // Setze failScore für die nächste Zeile zurück
-            failScore = 0;
-        }
+
+		 }
+
+	}
+
     }
-
-    return 0;
-}
-
